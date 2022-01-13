f516dfeea5bbba26ba3d627868ae0d1ca5df5eb8
noah <noah.simma@htldornbirn.at>
2022-01-13 13:21:18 +0100

    ….

---
+++ aufgaben wh css/js.basics/variablenoperatoren.js
@@ -0,0 +1,22 @@
+console.log("hallo, iiii");
+//variablen
+let a =7;
+let b= 8
+let c = a + b;
+console.log("a hat den Wert" + a);
+console.log("b hat den Wert" + b);
+console.log("a+b=" +c);
+
+c = b-a; console.log("a-b=" +c);
+c = 10; console.log("c=" +c);
+
+let result= a+b+c;
+console.log("das Ergebnis ist" + result);
+//aufgaben
+result = (a+b)/c; console.log("das Ergebnis ist" + result);
+result = c-a+b; console.log("das Ergebnis ist" + result);
+result = a+b+c * 3; console.log("das Ergebnis ist" + result);
+result = (a+b+c)/4; console.log("das Ergebnis ist" + result);
+a=3; console.log("a ist nun" +a);
+b=a+3; console.log("b ist nun" +b);
+c=c-3; console.log("c ist nun" +c);
